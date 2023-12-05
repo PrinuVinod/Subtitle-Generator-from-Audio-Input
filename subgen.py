8473ff2c890f5d01a866f3b5c2a21917a7e2e61d
Prinu Vinod Nair <71308351+PrinuVinod@users.noreply.github.com>
2023-12-05 13:40:32 +0530

    updating files

---
+++ subgen.py
@@ -0,0 +1,26 @@
+import speech_recognition as sr
+
+def generate_subtitles(audio_path):
+    recognizer = sr.Recognizer()
+
+    with sr.AudioFile(audio_path) as source:
+        print("Processing audio file...")
+        audio_data = recognizer.record(source)
+
+    try:
+        print("Transcribing audio...")
+        subtitles = recognizer.recognize_google(audio_data)
+        print("Subtitles generated successfully:\n")
+        print(subtitles)
+
+    except sr.UnknownValueError:
+        print("Speech Recognition could not understand audio")
+
+    except sr.RequestError as e:
+        print(f"Could not request results from Google Web Speech API; {e}")
+
+if __name__ == "__main__":
+    # Replace 'your_audio_file.wav' with the path to your audio file
+    audio_path = 'audio.wav'
+    
+    generate_subtitles(audio_path)
